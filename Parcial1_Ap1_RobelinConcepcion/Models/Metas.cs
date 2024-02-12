8dc1923235e59a0bec8e938448b52c67283697ae
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-02-12 18:19:44 -0400

    Creé la clase Metas en la carpeta Models

---
+++ Parcial1_Ap1_RobelinConcepcion/Models/Metas.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1_Ap1_RobelinConcepcion.Models
+{
+    public class Metas
+    {
+        [Key]
+        public int MetaId { get; set; }
+
+        [Required]
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "La Descripción es Obligatoria.")]
+        public string? Descripción { get; set; }
+
+        [Required(ErrorMessage = "El Monto es obligatorio.")]
+        public decimal Monto { get; set; }
+    }
+}
