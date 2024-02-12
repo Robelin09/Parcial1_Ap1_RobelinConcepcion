4f47530a381527954bc050d98aa475e86e3624de
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-02-12 18:32:26 -0400

    Creé la carpeta DAL con la clase Contexto

---
+++ Parcial1_Ap1_RobelinConcepcion/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_Ap1_RobelinConcepcion.Models;
+
+namespace Parcial1_Ap1_RobelinConcepcion.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Metas> Metas { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
